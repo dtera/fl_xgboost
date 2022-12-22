33f65b79158fd419531587445feb40f826c0defc
dterazhao <dterazhao@tencent.com>
2022-12-22 23:59:10 +0800

    [feat]: add federated xgboost

--- fedxgb/include/comm/grpc/XgbServiceRegistry.h
+++ fedxgb/include/comm/grpc/XgbServiceRegistry.h
@@ -2,34 +2,51 @@
 // Created by HqZhao on 2022/12/22.
 //
 
-#pragma once
+#ifndef COMM_GRPC_XGBSERVICEREGISTRY_H_
+#define COMM_GRPC_XGBSERVICEREGISTRY_H_
 
 #include "comm/grpc/XgbClient.h"
 #include "comm/grpc/XgbServer.h"
 #include "dmlc/registry.h"
 
-using namespace dmlc;
-
-#define REGISTER_XGB_SERVEICE(ServiceName)                                                         \
-  struct ServiceName##Factory                                                                      \
-      : public dmlc::FunctionRegEntryBase<ServiceName##Factory, std::function<ServiceName *()> > { \
-  };                                                                                               \
-                                                                                                   \
-  DMLC_REGISTRY_REGISTER(ServiceName##Factory, ServiceName##Factory, ServiceName).set_body([]() {  \
-    return new ServiceName();                                                                      \
+#define REGISTER_XGB_SERVEICE(ServiceName)                                                        \
+  DMLC_REGISTRY_REGISTER(ServiceName##Factory, ServiceName##Factory, ServiceName).set_body([]() { \
+    return new ServiceName();                                                                     \
   })
 
-struct XgbServiceServerFactory;
+struct XgbServiceServerFactory
+    : public dmlc::FunctionRegEntryBase<XgbServiceServerFactory,
+                                        std::function<XgbServiceServer*()> > {};
 
-struct XgbServiceClientFactory;
+struct XgbServiceClientFactory
+    : public dmlc::FunctionRegEntryBase<XgbServiceClientFactory,
+                                        std::function<XgbServiceClient*()> > {};
 
-DMLC_REGISTRY_ENABLE(XgbServiceServerFactory);
+template <>
+dmlc::Registry<XgbServiceServerFactory>* dmlc::Registry<XgbServiceServerFactory>::Get() {
+  static Registry<XgbServiceServerFactory> inst;
+  return &inst;
+}
 
-DMLC_REGISTRY_ENABLE(XgbServiceClientFactory);
+template <>
+dmlc::Registry<XgbServiceClientFactory>* dmlc::Registry<XgbServiceClientFactory>::Get() {
+  static Registry<XgbServiceClientFactory> inst;
+  return &inst;
+}
 
-REGISTER_XGB_SERVEICE(XgbServiceServer);
+static __attribute__((unused))
+XgbServiceServerFactory& __make_XgbServiceServerFactory_XgbServiceServer__ =
+    ::dmlc::Registry<XgbServiceServerFactory>::Get()
+        ->__REGISTER__("XgbServiceServer")
+        .set_body([]() { return new XgbServiceServer(); });
 
-REGISTER_XGB_SERVEICE(XgbServiceClient);
+static __attribute__((unused))
+XgbServiceClientFactory& __make_XgbServiceClientFactory_XgbServiceClient__ =
+    ::dmlc::Registry<XgbServiceClientFactory>::Get()
+        ->__REGISTER__("XgbServiceClient")
+        .set_body([]() { return new XgbServiceClient(); });
 
 #define FIND_XGB_SERVICE(ServiceName) \
   dmlc::Registry<ServiceName##Factory>::Find(#ServiceName)->body()
+
+#endif  // COMM_GRPC_XGBSERVICEREGISTRY_H_
