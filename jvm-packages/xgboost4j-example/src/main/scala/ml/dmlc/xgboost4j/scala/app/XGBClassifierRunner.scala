b5ae15cb043351dc48d224a6e04ca4c561a2e564
dterazhao <dterazhao@tencent.com>
2023-04-23 11:31:51 +0800

    [feat]: add federated xgboost

--- jvm-packages/xgboost4j-example/src/main/scala/ml/dmlc/xgboost4j/scala/app/XGBClassifierRunner.scala
+++ jvm-packages/xgboost4j-example/src/main/scala/ml/dmlc/xgboost4j/scala/app/XGBClassifierRunner.scala
@@ -1,3 +1,18 @@
+/*
+ Copyright (c) 2023 by Contributors
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 package ml.dmlc.xgboost4j.scala.app
 
 import ml.dmlc.xgboost4j.java.app.{AbstractSparkApp, ParamUtils}
@@ -15,9 +30,14 @@ object XGBClassifierRunner extends AbstractSparkApp {
     .setRawPredictionCol("rawPrediction")
     .setMetricName("areaUnderROC")
 
+  var local = false
+
+  override def isLocal: Boolean = local
+
   def main(args: Array[String]): Unit = {
     setXgbParams(ParamUtils.params)
     val params = ParamUtils.fromArgs(args)
+    local = if (params.contains("local")) params("local").toString.toBoolean else false
 
     val inputDF = spark.read.format(FED_LIBSVM).load(params("input_path").toString)
     val xgbClassifier = new XGBoostClassifier(params.toMap)
