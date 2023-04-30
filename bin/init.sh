18b7f8e8e7da688994d5dd3a4edb5946471bea20
dterazhao <dterazhao@tencent.com>
2023-04-30 16:13:51 +0800

    [feat]: add federated xgboost

--- bin/init.sh
+++ bin/init.sh
@@ -13,7 +13,8 @@ elif [ "$os_release" == "ubuntu" ]; then
   apt-get update && apt-get install -y cmake libssl-dev build-essential autoconf libtool pkg-config libc-ares-dev
 else
   yum -y install openssl cmake autoconf libtool pkg-config centos-release-scl devtoolset-7-gcc* \
-    openssl tbb tbb-devel boost boost-devel boost-build gtest-devel gmock-devel gmp-devel
+    tbb tbb-devel \
+    gtest-devel gmock-devel gmp-devel # boost boost-devel boost-build
 fi
 # =================================================================================================================
 # ==============================================Prepare End========================================================
@@ -23,15 +24,17 @@ fi
 # ==============================================Install gRPC Begin=================================================
 # =================================================================================================================
 ver=1.54.0
-echo "grpc version: v$ver"
-if [ "$1" != "" ]; then
+if [ "$1" != "" ] && [ "$1" != "clone" ]; then
   ver="$1"
 fi
+echo "grpc version: v$ver"
 
-rm -rf grpc && git clone --recurse-submodules -b "v$ver" --depth 1 --shallow-submodules https://github.com/grpc/grpc
+if [ "$1" == "clone" ]; then
+  rm -rf grpc && git clone --recurse-submodules -b "v$ver" --depth 1 --shallow-submodules https://github.com/grpc/grpc
+fi
 cd grpc || exit
 #git submodule update --init
-if [ "$ver" != "1.35.0" ]; then
+if [ "$ver" == "1.35.0" ]; then
   # Install absl
   printf "Trying to install abseil...\n"
   mkdir -p third_party/abseil-cpp/cmake/build && cd third_party/abseil-cpp/cmake/build || exit
@@ -78,7 +81,7 @@ else
   # sh cmake-linux.sh -- --skip-license --prefix=/usr/local/cmake && rm cmake-linux.sh
   # cmk=/usr/local/cmake/bin/cmake
   # git clone -b "$(curl -L https://grpc.io/release)" https://github.com/grpc/grpc
-  mkdir -p cmake/build && pushd cmake/build || exit
+  rm -rf cmake/build && mkdir -p cmake/build && pushd cmake/build || exit
   cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DgRPC_INSTALL=ON \
@@ -93,10 +96,10 @@ else
   make && make install
   cd - && cd ..
 fi
-export LD_LIBRARY_PATH="/usr/local/grpc/lib:/usr/local/grpc/lib64:$LD_LIBRARY_PATH"
-export LDFLAGS="-Wl,--copy-dt-needed-entries"
-echo "export LD_LIBRARY_PATH=/usr/local/grpc/lib:/usr/local/grpc/lib64:$LD_LIBRARY_PATH" >>~/.bashrc
-echo "export LDFLAGS=-Wl,--copy-dt-needed-entries" >>~/.bashrc
+grep -q 'export LD_LIBRARY_PATH' ~/.bashrc || export LD_LIBRARY_PATH="/usr/local/grpc/lib:/usr/local/grpc/lib64:$LD_LIBRARY_PATH"
+grep -q 'export LDFLAGS' ~/.bashrc || export LDFLAGS="-Wl,--copy-dt-needed-entries"
+grep -q 'export LD_LIBRARY_PATH' ~/.bashrc || echo "export LD_LIBRARY_PATH=/usr/local/grpc/lib:/usr/local/grpc/lib64:$LD_LIBRARY_PATH" >>~/.bashrc
+grep -q 'export LDFLAGS' ~/.bashrc || echo "export LDFLAGS=-Wl,--copy-dt-needed-entries" >>~/.bashrc
 # =================================================================================================================
 # ==============================================Install gRPC End===================================================
 # =================================================================================================================
